16798a911da417c0257e539b7ff3a6c9017473ca
Takuma HANATANI <mail@potato4d.me>
2025-07-26 20:28:38 +0900

    feat: update synchro-cup

--- src/masterdata.ts
+++ src/masterdata.ts
@@ -10,28 +10,31 @@ export const SAMPLE_DATA = [
   {
     name: 'Tier2',
     decks: [
-      { name: '青眼', image: '/static/deckimages/blue-eyes.png' },
-      { name: 'クリストロン', image: '/static/deckimages/crystron.png' },
+      { name: 'スネークアイ', image: '/static/deckimages/snake-eye.png' },
+      { name: '千年M∀LICE', image: '/static/deckimages/millennium-malice.png' },
+      { name: '天盃龍', image: '/static/deckimages/tenpai-dragon.png' },
     ],
   },
   {
     name: 'Tier3',
     decks: [
-      { name: '海皇', image: '/static/deckimages/atlantean.png' },
+      { name: '青眼', image: '/static/deckimages/blue-eyes.png' },
+      { name: 'メタビート', image: '/static/deckimages/metabeat.png' },
       { name: '白き森', image: '/static/deckimages/white-forest.png' },
-      { name: 'メメント', image: '/static/deckimages/mementotlan.png' },
     ],
   },
   {
     name: 'Tier4',
     decks: [
-      { name: '天盃龍', image: '/static/deckimages/tenpai-dragon.png' },
+      { name: '海皇', image: '/static/deckimages/atlantean.png' },
+      { name: '粛声', image: '/static/deckimages/voiceless_voice.png' },
+      { name: 'メメント', image: '/static/deckimages/mementotlan.png' },
     ],
   },
 ]
 
 export const INITIAL_AVAILABLE_DECKS: Deck[] = [
-      { name: 'スネークアイ', image: '/static/deckimages/snake-eye.png' },
+  { name: 'クリストロン', image: '/static/deckimages/crystron.png' },
   { name: 'ラビュリンス', image: '/static/deckimages/labrynth.png' },
   { name: 'P.U.N.K.', image: '/static/deckimages/punk.png' },
   { name: 'センチュリオン', image: '/static/deckimages/centur-ion.png' },
@@ -41,13 +44,13 @@ export const INITIAL_AVAILABLE_DECKS: Deck[] = [
   { name: '烙印', image: '/static/deckimages/branded.png' },
   { name: 'ティアラメンツ', image: '/static/deckimages/tearlaments.png' },
   { name: 'ドライトロン', image: '/static/deckimages/drytron.png' },
+  { name: '氷結界', image: '/static/deckimages/ice-barrier.png' },
+  { name: 'エルドリッチ', image: '/static/deckimages/eldlich.png' },
   { name: '竜剣士', image: '/static/deckimages/dracoslayer.png' },
   { name: '60GS', image: '/static/deckimages/60gs.png' },
   { name: '炎王スネークアイ', image: '/static/deckimages/fireking-snake-eye.png' },
   { name: 'R-ACE', image: '/static/deckimages/r-ace.png' },
-  { name: '粛声', image: '/static/deckimages/voiceless_voice.png' },
   { name: 'ギミック・パペット', image: '/static/deckimages/gimmick-puppet.png' },
-  { name: 'メタビート', image: '/static/deckimages/metabeat.png' },
   { name: 'マナドゥム', image: '/static/deckimages/mannadium.png' },
   { name: 'キマイラ', image: '/static/deckimages/chimera.png' },
   { name: '覇王幻奏', image: '/static/deckimages/supreme-melodious.png' },
